acfdea9c8d9db374aa101a67362adc57f6f1a9a5
tianhewang <tianhe.wang@media-smart.cn>
2020-01-22 10:33:48 +0800

    update dataset

--- vedaseg/datasets/voc.py
+++ vedaseg/datasets/voc.py
@@ -1,14 +1,12 @@
-from torch.utils.data import Dataset
-import pandas as pd
-import torch
+import logging
+import os
+
+import cv2
 import numpy as np
 from PIL import Image
-import os
-import cv2
-import logging
 
-from .registry import DATASETS
 from .base import BaseDataset
+from .registry import DATASETS
 
 logger = logging.getLogger()
 
@@ -17,6 +15,7 @@ logger = logging.getLogger()
 class VOCDataset(BaseDataset):
     """
     """
+
     def __init__(self, imglist_name, root, transform):
         super().__init__()
 
@@ -33,11 +32,11 @@ class VOCDataset(BaseDataset):
         mask_fp = os.path.join(self.root, 'EncodeSegmentationClass', imgname) + '.png'
         img = cv2.imread(img_fp).astype(np.float32)
         mask = np.array(Image.open(mask_fp), dtype=np.float32)
-        #mask = np.zeros((img.shape[0], img.shape[1])) #np.array(Image.open(mask_fp))
+        # mask = np.zeros((img.shape[0], img.shape[1])) #np.array(Image.open(mask_fp))
         image, mask = self.process(img, mask)
         mask = mask.long()
 
-        return image, mask, img_fp, mask_fp
+        return image, mask, imgname
 
     def __len__(self):
         return len(self.imglist)
@@ -48,4 +47,3 @@ class VOCDataset(BaseDataset):
             for line in fd:
                 ll.append(line.strip())
         return ll
-
