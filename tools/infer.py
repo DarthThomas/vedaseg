3efa7bd81602ece3ca7f4be22455b8c229710404
DarthThomas <wth625361486@gmail.com>
2020-03-09 21:52:13 +0800

    debug:checkmask

--- tools/infer.py
+++ tools/infer.py
@@ -14,10 +14,9 @@ from vedaseg.assemble import assemble
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Use trained semantic segmenter')
-    parser.add_argument('config', help='train config file path', default='configs/horc_1_50_d3p_481.py')
-    parser.add_argument('checkpoint', help='train config file path', default='checkpoints/epoch_50.pth')
-    parser.add_argument('img_dir', help='infer image path',
-                        default='/media/yuhaoye/DATA7/git/Seg_Vis/mask_ignore/JPEGImages/20191206_110208_2_3.jpg')
+    parser.add_argument('config', help='train config file path')
+    parser.add_argument('checkpoint', help='train config file path')
+    parser.add_argument('img_dir', help='infer image path')
     args = parser.parse_args()
     return args
 
@@ -49,6 +48,7 @@ def get_plot(img, pd, vis_mask=True, vis_contour=True):
     plt.subplot(122)
     plt.title('image with prediction')
     if vis_mask:
+        print('where is the fucking mask')
         img = image_overlay(img, pd, [0, 255, 0])
     if vis_contour:
         img = get_contours(img, pd)
@@ -63,9 +63,7 @@ def image_overlay(image, mask, color):
     # pred = mask * 255
     heat_map = np.zeros_like(image)
     heat_map[mask] = color
-    # heat_map = cv2.applyColorMap(pred.astype(np.uint8), cv2.COLORMAP_JET)
-    img_pd = image.copy()
-    fin = cv2.addWeighted(heat_map, -0.5, img_pd, 0.8, 0)
+    fin = cv2.addWeighted(heat_map, -0.5, image, 0.8, 0)
     return fin
 
 
